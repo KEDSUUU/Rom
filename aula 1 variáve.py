50142ef402be28e04966a069b6c2c2aa439ecd7d
KED RB <contastarcio24@gmail.com>
2024-06-15 12:29:06 -0300

    update aula 1 variáve.py

--- aula 1 variáve.py
+++ aula 1 variáve.py
@@ -4,10 +4,3 @@ def soma(num1, num2):
 
 resultado = soma(5, 3)
 print(resultado)
-
-def soma(num1, num2):  # Define uma função chamada soma que recebe dois parâmetros, num1 e num2
-    valor = num1 + num2  # Calcula a soma dos dois parâmetros e armazena o resultado na variável valor
-    return valor  # Retorna o valor calculado para onde a função foi chamada
-
-resultado = soma(5, 3)  # Chama a função soma com os argumentos 5 e 3, e armazena o resultado na variável resultado
-print(resultado)  # Imprime o valor retornado pela função soma, que é 8 neste caso
