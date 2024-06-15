377774d1fca1dd99b0b936ecfda66a34f12f2d7a
KED RB <contastarcio24@gmail.com>
2024-06-15 14:51:43 -0300

    update aula 1 variáve.py

--- aula 1 variáve.py
+++ aula 1 variáve.py
@@ -1,17 +1,9 @@
-def soma(num1, num2):
-    valor = num1 + num2
-    return valor
-
-resultado = soma(5, 3)
-print(resultado)
-
-
 def tt(n1, n2):
   teste = n1 + n2
   return teste
   
-igual = soma(3.3, 4.4)
+igual = tt(3.3, 4.4)
 print (igual)
-  
+
   
   
